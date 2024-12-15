a5a04dae2cd64277feaf7fd6bd5ac6c45aedbf6c
Vladimír Ulman <ulman@fi.muni.cz>
2024-12-15 13:54:43 +0100

    ADD: Labkit checks for SAMJ availability and may install "SAMJ button", or...
    ...prints a notice message (that SAMJ could be installed simply via Fiji update site).

--- src/main/java/sc/fiji/labkit/ui/BasicLabelingComponent.java
+++ src/main/java/sc/fiji/labkit/ui/BasicLabelingComponent.java
@@ -45,6 +45,7 @@ import sc.fiji.labkit.ui.models.ImageLabelingModel;
 import sc.fiji.labkit.ui.panel.LabelToolsPanel;
 import net.miginfocom.swing.MigLayout;
 import org.scijava.ui.behaviour.util.AbstractNamedAction;
+import sc.fiji.labkit.ui.utils.LabkitUtils;
 
 import javax.swing.*;
 import java.awt.*;
@@ -123,14 +124,10 @@ public class BasicLabelingComponent extends JPanel implements AutoCloseable {
 			bdvHandle, model, actionsAndBehaviours);
 		final SelectLabelController selectLabelController =
 			new SelectLabelController(bdvHandle, model, actionsAndBehaviours);
+		final SamjFill samjFill = LabkitUtils.isSamjAvailable() ? new SamjFill(bdvHandle, model) : null;
 		final JPanel toolsPanel = new LabelToolsPanel(brushController,
-			floodFillController, selectLabelController, planarModeController);
+			floodFillController, selectLabelController, planarModeController, samjFill);
 		actionsAndBehaviours.addAction(new ChangeLabel(model));
-
-		//makes the SAM addon available
-		//TODO: init only if such class is available on JVM path, in which case it should add an icon...
-		new SamjFill(bdvHandle, model);
-
 		return toolsPanel;
 	}
 
