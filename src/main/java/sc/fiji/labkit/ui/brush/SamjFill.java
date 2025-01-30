09e921c73d101c3b500342bbe93700a49e39dac9
Vladimír Ulman <ulman@fi.muni.cz>
2025-01-30 23:53:51 +0100

    ADD: SamjFill.LabelPresenceIndicatorAtGlobalCoord and installs the...
    ...samj.installPerSlicesTrackingPromptBehaviour()
    and installRepeatPromptOnNextSliceBehaviour().
    
    The class' indicator isPresent() may fast fail when
    no 'label' is selected. Which is, for example, the case
    when this code is installed as Labkit is yet not fully initiated
    and cannot provide model.labeling(). On the other hand,
    even when user doesn't click anything/doesn't choose any from
    the labels, the first one is returned with model.selectedLabel()
    and so it seems that there will always be some label available
    anytime later during the operation of the application.

--- src/main/java/sc/fiji/labkit/ui/brush/SamjFill.java
+++ src/main/java/sc/fiji/labkit/ui/brush/SamjFill.java
@@ -4,10 +4,13 @@ import ai.nets.samj.bdv.promptresponders.SamjResponder;
 import ai.nets.samj.communication.model.SAM2Tiny;
 import ai.nets.samj.util.PlanarShapesRasterizer;
 import bdv.interactive.prompts.BdvPrompts;
+import bdv.interactive.prompts.BdvPrompts3D;
 import bdv.interactive.prompts.planarshapes.PlanarPolygonIn3D;
 import bdv.util.BdvHandle;
 import bdv.viewer.SourceAndConverter;
 import net.imglib2.RandomAccess;
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.roi.labeling.LabelingType;
 import net.imglib2.type.numeric.real.FloatType;
@@ -32,6 +35,12 @@ public class SamjFill {
 		samj.addPolygonsConsumer( this.new SamjLabeller() );
 		samj.stopPrompts(); //in Labkit, it should start disabled by default
 
+		if (model.labeling().get().randomAccess().numDimensions() > 2) {
+			System.out.println("Detected 3D image, enabling 'perSlices' SAMJ annotations.");
+			samj.installRepeatPromptOnNextSliceBehaviour();
+			samj.installPerSlicesTrackingPromptBehaviour(new LabelPresenceIndicatorAtGlobalCoord());
+		}
+
 		this.bdv = bdv;
 		this.model = model;
 	}
@@ -39,6 +48,36 @@ public class SamjFill {
 	public String getBdvName() {
 		return bdv.getViewerPanel().getName();
 	}
+
+
+	protected class LabelPresenceIndicatorAtGlobalCoord implements BdvPrompts3D.LabelPresenceIndicatorAtGlobalCoord {
+		LabelPresenceIndicatorAtGlobalCoord() { label = null; }
+
+		@Override
+		public void prepareForQueryingSession() {
+			label = model.selectedLabel().get();
+			ra = model.labeling().get().randomAccess();
+			globalToImageT = model.labelTransformation().inverse(); // model.labelTransformation() is giving image to global
+			System.out.println("Labkit's indicator will be checking label: "+label.name());
+		}
+
+		Label label;
+		RandomAccess<LabelingType<Label>> ra;
+		AffineTransform3D globalToImageT;
+		RealPoint pos = new RealPoint(3);
+
+		@Override
+		public boolean isPresent(RealLocalizable position) {
+			if (label == null) return false;
+
+			globalToImageT.apply(position, pos);
+			return ra.setPositionAndGet( Math.round(pos.getDoublePosition(0)), //TODO what if outside the label image??
+			                             Math.round(pos.getDoublePosition(1)),
+			                             Math.round(pos.getDoublePosition(2)) )
+					  .contains(label);
+		}
+	}
+
 
 	public final BdvPrompts<?,FloatType> samj;
 	final BdvHandle bdv;
