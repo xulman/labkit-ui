20ce9dc8352f81ddc28b8001f9a2597159e86ec3
Vladimír Ulman <ulman@fi.muni.cz>
2025-03-24 14:43:50 +0100

    FIX: SamjFill now always initiates to a valid SAMJ network:
    Either by taking the first network from the available ones
    if there is such, else by installing a "network" (PromptsProcessor)
    that only warns on the screen that no real network is used.
    
    In any case, the reporting here now always starts with "Labkit: ..."

--- src/main/java/sc/fiji/labkit/ui/brush/SamjFill.java
+++ src/main/java/sc/fiji/labkit/ui/brush/SamjFill.java
@@ -1,7 +1,7 @@
 package sc.fiji.labkit.ui.brush;
 
 import ai.nets.samj.bdv.promptresponders.SamjResponder;
-import ai.nets.samj.communication.model.SAM2Tiny;
+import ai.nets.samj.util.AvailableNetworksFactory;
 import ai.nets.samj.util.PlanarShapesRasterizer;
 import bdv.interactive.prompts.BdvPrompts;
 import bdv.interactive.prompts.BdvPrompts3D;
@@ -17,6 +17,8 @@ import net.imglib2.type.numeric.real.FloatType;
 import sc.fiji.labkit.ui.labeling.Label;
 import sc.fiji.labkit.ui.models.LabelingModel;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class SamjFill {
@@ -31,12 +33,30 @@ public class SamjFill {
 				  "SAMJ-accelerated annotator",
 				  new FloatType(),
 				  false);
-		samj.addPromptsProcessor( new SamjResponder<>( new SAM2Tiny() ) );
+		final AvailableNetworksFactory netFactory = new AvailableNetworksFactory();
+		final List<String> availableModels = netFactory.availableModels();
+		if (availableModels.size() > 0) {
+			//grab the first available network
+			samj.addPromptsProcessor(new SamjResponder<>( netFactory.getModel(availableModels.get(0)) ));
+			availableModels.forEach(netName -> System.out.println("Labkit: Detected available SAMJ: "+netName));
+			System.out.println("Labkit: Using initially the first-one listed. Change it by double-clicking the SAMJ icon in Labkit.");
+		} else {
+			samj.addPromptsProcessor((BdvPrompts.PromptsProcessor)(prompt, hasViewChangedSinceBefore) -> {
+				bdv.getViewerPanel().showMessage("No SAMJ model installed.");
+				System.out.println("Labkit: No SAMJ model installed yet, doing nothing. Please, double-click the SAMJ icon in Labkit to configure SAMJ.");
+				return Collections.emptyList();
+			});
+			//samj.stopPrompts(); -- could have been used too but then no prompting would occur and
+			//thus the message above would never be displayed; this behaviour (stopping prompts on
+			//an invalid selected model) is used in the configuration dialog (dbl-clicking the SAMJ
+			//icon) but there user has already discovered that the icon can be dbl-clicked (so, no
+			//warning message needs to come out)
+		}
 		samj.addPolygonsConsumer( this.new SamjLabeller() );
 		samj.stopPrompts(); //in Labkit, it should start disabled by default
 
 		if (model.labeling().get().randomAccess().numDimensions() > 2) {
-			System.out.println("Detected 3D image, enabling 'perSlices' SAMJ annotations.");
+			System.out.println("Labkit: Detected 3D image, enabling 'perSlices' SAMJ annotations.");
 			samj.installRepeatPromptOnNextSliceBehaviour();
 			samj.installPerSlicesTrackingPromptBehaviour(new LabelPresenceIndicatorAtGlobalCoord());
 		}
