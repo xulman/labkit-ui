bb78d9b58adde0a699efa2d8a75ba943094cf1f3
Vladimir Ulman (@Win11) <ulman@fi.muni.cz>
2025-11-24 01:09:33 +0100

    ADD: FakeSegFiller that draws rectangle inside the prompt, and builds...
    ...on the AbstractSegFill, a demo version...

---
+++ src/main/java/sc/fiji/labkit/ui/brush/FakeSegFiller.java
@@ -0,0 +1,42 @@
+package sc.fiji.labkit.ui.brush;
+
+import bdv.interactive.prompts.planarshapes.PlanarRectangleIn3D;
+import bdv.util.BdvHandle;
+import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
+import sc.fiji.labkit.ui.models.LabelingModel;
+
+public class FakeSegFiller extends AbstractSegFill {
+    public FakeSegFiller(BdvHandle bdv, LabelingModel model) {
+        super(bdv, model);
+
+        //TODO: here own extras should be initiated
+        System.out.println("Creating FakeSegFiller....");
+    }
+
+    @Override
+    void segment(PlanarRectangleIn3D<FloatType> prompt, Img<UnsignedByteType> fillThisMask) {
+        //let's fill a small rectangle inside the prompt
+
+        Interval srcI = prompt.getBbox2D();
+        long xOneThird = (srcI.max(0) - srcI.min(0) + 1) / 3;
+        long yOneThird = (srcI.max(1) - srcI.min(1) + 1) / 3;
+
+        Interval tgtI = new FinalInterval(
+            new long[] {
+                srcI.min(0) + xOneThird,
+                srcI.min(1) + yOneThird
+            }, new long[] {
+                srcI.min(0) + 2*xOneThird,
+                srcI.min(1) + 2*yOneThird
+            } );
+
+        //zero first the full output image, then non-zero certain area
+        fillThisMask.forEach(UnsignedByteType::setZero);
+        Views.interval(fillThisMask, tgtI).forEach(UnsignedByteType::setOne);
+    }
+}
