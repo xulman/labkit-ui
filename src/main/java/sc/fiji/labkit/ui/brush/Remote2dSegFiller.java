cd316b7bd2e3c109b42ca26a2337a29c01b986b7
Vladimir Ulman (@Win11) <ulman@fi.muni.cz>
2025-11-26 20:08:33 +0100

    ADD: Remote2dSegFiller and Fillers

---
+++ src/main/java/sc/fiji/labkit/ui/brush/Remote2dSegFiller.java
@@ -0,0 +1,54 @@
+package sc.fiji.labkit.ui.brush;
+
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.IntegerType;
+import net.imglib2.type.numeric.RealType;
+import sc.fiji.labkit.ui.utils.RemoteSegmenterCommunications;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Remote2dSegFiller {
+	public Remote2dSegFiller(final String url) throws IOException {
+		this.url = url;
+		this.updateAvailableMethods();
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void updateAvailableMethods() throws IOException {
+		lastSeenAvailableMethods.clear();
+		lastSeenAvailableMethods.addAll( RemoteSegmenterCommunications.listAvailableNetworks(url) );
+		selectedMethod = !lastSeenAvailableMethods.isEmpty() ?
+				lastSeenAvailableMethods.get(0) : NO_METHOD_SELECTED;
+	}
+
+	public List<String> reportAvailableMethods() {
+		return Collections.unmodifiableList( lastSeenAvailableMethods );
+	}
+
+	private final String url;
+	private final List<String> lastSeenAvailableMethods = new ArrayList<>(0);
+
+	private String selectedMethod;
+	public final String NO_METHOD_SELECTED = "nothing.selected";
+
+	public String selectMethod(final int index) {
+		selectedMethod = (index < 0 || index >= lastSeenAvailableMethods.size()) ?
+				NO_METHOD_SELECTED : lastSeenAvailableMethods.get(index);
+		return selectedMethod;
+	}
+
+	public <IT extends RealType<IT>, MT extends IntegerType<MT>>
+	void segment(final Img<IT> inputImg, final Img<MT> maskImg) {
+		try {
+			RemoteSegmenterCommunications.runRemoteSegmentation2D(url, inputImg, maskImg, selectedMethod);
+		} catch (IOException e) {
+			System.out.println("ERROR segmenting at "+url+" with method >>"+selectedMethod+"<<:\n"+e.getMessage());
+			Remote2dSegFillers.zeroMask(maskImg);
+		}
+	}
+}
---
+++ src/main/java/sc/fiji/labkit/ui/brush/Remote2dSegFillers.java
@@ -0,0 +1,83 @@
+package sc.fiji.labkit.ui.brush;
+
+import bdv.interactive.prompts.planarshapes.PlanarRectangleIn3D;
+import bdv.util.BdvHandle;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.real.FloatType;
+import sc.fiji.labkit.ui.models.LabelingModel;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Remote2dSegFillers extends AbstractSegFill {
+	public Remote2dSegFillers(BdvHandle bdv, LabelingModel model) {
+		super(bdv, model);
+	}
+
+	private final List<Remote2dSegFiller> servers = new ArrayList<>(20);
+
+	public void setPoolOfServers(List<String> serverURLs) {
+		servers.clear();
+		serverURLs.forEach(url -> {
+			try {
+				servers.add( new Remote2dSegFiller(url) );
+			} catch (IOException e) {
+				System.out.println("ERROR talking to "+url+":\n"+e.getMessage());
+			}
+		});
+	}
+
+	public void randomizeOrderOfServers() {
+		Collections.shuffle( servers );
+	}
+
+	public List<Remote2dSegFiller> getPoolOfServers() {
+		return servers;
+	}
+
+
+	public void updateListOfMethodsOnAllServers() {
+		servers.forEach(server -> {
+			try {
+				server.updateAvailableMethods();
+			} catch (IOException e) {
+				System.out.println("ERROR updating methods at "+server.getUrl()+":\n"+e.getMessage());
+			}
+		});
+	}
+
+	public void setSameMethodOnAllServers(final String method) {
+		servers.forEach(server -> {
+			List<String> methods = server.reportAvailableMethods();
+			int j = -1;
+			for (int i = 0; i < methods.size(); ++i) {
+				if (method.equals( methods.get(i) )) {
+					j = i;
+					break;
+				}
+			}
+			//NB: selects "nothing" (because of j=-1) if 'method' not found among the server's available ones
+			server.selectMethod(j);
+			if (j == -1) System.out.println("ERROR method >>"+method+"<< not available at "+server.getUrl());
+		});
+	}
+
+
+	@Override
+	void segment(PlanarRectangleIn3D<FloatType> prompt, Img<UnsignedByteType> fillThisMask) {
+		if (servers.isEmpty()) {
+			zeroMask(fillThisMask);
+		} else {
+			//TODO: round robin!!!
+			servers.get(0).segment(prompt.getViewImage2D(), fillThisMask);
+		}
+	}
+
+	public static <MT extends RealType<MT>> void zeroMask(Img<MT> mask) {
+		mask.forEach(MT::setZero);
+	}
+}
