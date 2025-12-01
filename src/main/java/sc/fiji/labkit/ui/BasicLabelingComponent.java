00894e839e4a3d8ca29d3b7e583a9620c4d0c1c8
Vladimír Ulman <ulman@fi.muni.cz>
2025-12-01 18:48:44 +0100

    COS,FIX: SAMJ button made available in Labkit again

--- src/main/java/sc/fiji/labkit/ui/BasicLabelingComponent.java
+++ src/main/java/sc/fiji/labkit/ui/BasicLabelingComponent.java
@@ -142,8 +142,7 @@ public class BasicLabelingComponent extends JPanel implements AutoCloseable {
 		//so that a downstream code that works solely with that BDV
 		//could provide some identification (to which BDV it belongs)
 		bdvHandle.getViewerPanel().setName(this.dialogBoxOwner.getTitle());
-		//final SamjFill samjFill = LabkitUtils.isSamjAvailable() ? new SamjFill(bdvHandle, model) : null;
-		final SamjFill samjFill = null;
+		final SamjFill samjFill = LabkitUtils.isSamjAvailable() ? new SamjFill(bdvHandle, model) : null;
 		final Remote2dSegFillers remoteSegFill = LabkitUtils.isBdvPromptingAvailable() ? new Remote2dSegFillers(bdvHandle, model) : null;
 		if (remoteSegFill != null) {
 			remoteSegFill.setPoolOfServers( Arrays.asList("http://localhost:8000") );
